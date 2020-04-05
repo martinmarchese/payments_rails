71a6be3bed27a2729d994639397fc0b078f0c05b
martin marchese <martinmarchese@gmail.com>
2020-04-05 00:52:47 -0300

    add dt

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,16 +4,4 @@ class Payment < ApplicationRecord
   validates :date, presence: true
   validates :amount, presence: true
   validates :name, presence: true
-
-  filterrific(
-   default_filter_params: { sorted_by: 'date' },
-   available_filters: [
-     :sorted_by,
-     :search_query,
-     :with_country_id,
-     :with_created_at_gte
-   ]
- )
- # define ActiveRecord scopes for
- # :search_query, :sorted_by, :with_country_id, and :with_created_at_gte
 end
